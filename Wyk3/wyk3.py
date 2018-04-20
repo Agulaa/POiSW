aa46e7e746aaa8443e85a3e02ae568105f75af70
Agulaa <agnieszkaxrusin@gmail.com>
2018-04-20 15:22:04 +0200

    Update hougha_transform method

--- Wyk3/wyk3.py
+++ Wyk3/wyk3.py
@@ -132,16 +132,14 @@ def descriptor_HoG():
 
 def Hougha_transform():
 
-    img = cv2.imread('3.jpg')
+    img = cv2.imread('bloki.PNG')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Input', gray)
     edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-    min_line_length = 100
-    max_line_gap = 10
-
-    lines = cv2.HoughLinesP(edges, 1,np.pi/180, 100, min_line_length, max_line_gap )
+    minLineLength = 100
+    maxLineGap = 10
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength, maxLineGap)
     for x1, y1, x2, y2 in lines[0]:
-        cv2.line(img, (x1,y1), (x2,y2), (0,255,0), 2)
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
     cv2.imshow('Output',img)
     cv2.waitKey(0)
